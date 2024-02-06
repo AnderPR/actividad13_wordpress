00b96c2f8390a43bf32e45da5abff4c944885955
AnderPR <92815931+AnderPR@users.noreply.github.com>
2024-02-06 15:38:51 +0100

    Update install_lamp.sh

--- scripts/install_lamp.sh
+++ scripts/install_lamp.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-# Cargar variables de entorno desde el archivo .env
-export $(grep -v '^#' .env | xargs)
+
+# Verificar si existe el archivo .env y si existe cargar las variables
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+else
+  echo "El archivo .env no se encuentra. Asegúrate de tenerlo con las variables necesarias."
+  exit 1
+fi
 
 # Instalación de Apache y PHP
 sudo apt update
+sudo apt upgrade -y
 sudo apt install -y apache2
 sudo apache2 -v
+
 sudo apt install -y php libapache2-mod-php php-mysql
 
 # Edición del sitio por defecto de Apache
@@ -17,6 +25,7 @@ echo "<VirtualHost *:80>
 	ErrorLog \${APACHE_LOG_DIR}/error.log
 	CustomLog \${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>" | sudo tee /etc/apache2/sites-available/000-default.conf > /dev/null
+
 
 # Reiniciar Apache
 sudo systemctl restart apache2
@@ -33,6 +42,7 @@ FLUSH PRIVILEGES;
 QUIT
 EOF
 
+
 # Instalación de PHPMyAdmin
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PHPMYADMIN_PASSWORD"
@@ -40,6 +50,8 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PHPMYADMIN_PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 sudo apt install -y phpmyadmin php-mbstring php-zip php-gd php-json php-curl
+
+
 
 # Reiniciar Apache después de la instalación de PHPMyAdmin
 sudo systemctl restart apache2
